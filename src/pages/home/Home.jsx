d047b84eff0e17513e6766a17e94c922161a54e1
Kamoliddin Mirzaboyev <Kamoliddinmirzaboyev05@gmail.com>
2025-04-02 16:56:59 +0500

    update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -74,129 +74,19 @@ function Home() {
                   </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                  <div className="productCard">
-                    <div className="productImg">
-                      <img src="/burger.png" alt="" />
-                    </div>
-                    <div className="productInfo">
-                      <h3>Big burger</h3>
-                      <div className="productMainInfo">
-                        <p>
-                          {" "}
-                          <i className="fas fa-dollar"></i> 32.000
-                        </p>
-                        <p>
-                          {" "}
-                          <i className="fas fa-clock"></i> 10 min{" "}
-                        </p>
-                      </div>
-                      <div className="cardBtns">
-                        <button className="btn productCartBtn">
-                          Savatga qo'shish
-                        </button>
-                      </div>
-                    </div>
-                  </div>
+                  <ProductCard />
                 </SwiperSlide>
                 <SwiperSlide>
-                  <div className="productCard">
-                    <div className="productImg">
-                      <img src="/burger.png" alt="" />
-                    </div>
-                    <div className="productInfo">
-                      <h3>Big burger</h3>
-                      <div className="productMainInfo">
-                        <p>
-                          {" "}
-                          <i className="fas fa-dollar"></i> 32.000
-                        </p>
-                        <p>
-                          {" "}
-                          <i className="fas fa-clock"></i> 10 min{" "}
-                        </p>
-                      </div>
-                      <div className="cardBtns">
-                        <button className="btn productCartBtn">
-                          Savatga qo'shish
-                        </button>
-                      </div>
-                    </div>
-                  </div>
+                  <ProductCard />
                 </SwiperSlide>
                 <SwiperSlide>
-                  <div className="productCard">
-                    <div className="productImg">
-                      <img src="/burger.png" alt="" />
-                    </div>
-                    <div className="productInfo">
-                      <h3>Big burger</h3>
-                      <div className="productMainInfo">
-                        <p>
-                          {" "}
-                          <i className="fas fa-dollar"></i> 32.000
-                        </p>
-                        <p>
-                          {" "}
-                          <i className="fas fa-clock"></i> 10 min{" "}
-                        </p>
-                      </div>
-                      <div className="cardBtns">
-                        <button className="btn productCartBtn">
-                          Savatga qo'shish
-                        </button>
-                      </div>
-                    </div>
-                  </div>
+                  <ProductCard />
                 </SwiperSlide>
                 <SwiperSlide>
-                  <div className="productCard">
-                    <div className="productImg">
-                      <img src="/burger.png" alt="" />
-                    </div>
-                    <div className="productInfo">
-                      <h3>Big burger</h3>
-                      <div className="productMainInfo">
-                        <p>
-                          {" "}
-                          <i className="fas fa-dollar"></i> 32.000
-                        </p>
-                        <p>
-                          {" "}
-                          <i className="fas fa-clock"></i> 10 min{" "}
-                        </p>
-                      </div>
-                      <div className="cardBtns">
-                        <button className="btn productCartBtn">
-                          Savatga qo'shish
-                        </button>
-                      </div>
-                    </div>
-                  </div>
+                  <ProductCard />
                 </SwiperSlide>
                 <SwiperSlide>
-                  <div className="productCard">
-                    <div className="productImg">
-                      <img src="/burger.png" alt="" />
-                    </div>
-                    <div className="productInfo">
-                      <h3>Big burger</h3>
-                      <div className="productMainInfo">
-                        <p>
-                          {" "}
-                          <i className="fas fa-dollar"></i> 32.000
-                        </p>
-                        <p>
-                          {" "}
-                          <i className="fas fa-clock"></i> 10 min{" "}
-                        </p>
-                      </div>
-                      <div className="cardBtns">
-                        <button className="btn productCartBtn">
-                          Savatga qo'shish
-                        </button>
-                      </div>
-                    </div>
-                  </div>
+                  <ProductCard />
                 </SwiperSlide>
               </Swiper>
             </div>
